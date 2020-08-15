516e462a4082e49a0b6b54b0f81c99488f01e0ef
chenxiaowen <819339194@qq.com>
2020-08-15 23:45:08 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonType, ButtonSize} from './components/Button/button'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+       <Button disabled> Hello </Button>
+       <Button className={'aaa bbb'} onClick={e=>console.log(e.target)}> Hello </Button>
+       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello Primary </Button>
+       <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Hello Danger </Button>
+       <Button btnType={ButtonType.Link} href='http://www.baidu.com' target={'_self'}> Baidu Link </Button>
+       <Button btnType={ButtonType.Link} href='http://www.baidu.com' disabled> Baidu Link </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
